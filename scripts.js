937ca2e826487ba56f623fa0ab549a1689c7010a
Adrian Furman <fadikk367@gmail.com>
2019-10-26 22:20:15 +0200

    definately fix logic (not optimize)

--- scripts.js
+++ scripts.js
@@ -10,57 +10,16 @@ const list = document.querySelector('.list__list');
 
 
 
-function addListPosition(coords, index) {
 
-    const listItem = document.createElement('div');
-    listItem.className = 'list__item';
-
-    const itemHeader = document.createElement('div');
-    itemHeader.className = 'item__header';
-
-    const title = document.createElement('h3');
-    title.textContent = `${index + 1}) coordinates:`;
-    title.className = 'item__title';
-
-    const deleteBtn = document.createElement('img');
-    deleteBtn.src = './images/delete.png';
-    deleteBtn.className = 'item__deleteBtn';
-    deleteBtn.addEventListener('click', function (e) {
-        const idx = [...listItems].findIndex(el => el === this.parentNode.parentNode)
-        this.parentNode.parentNode.remove();
-        markers[idx].setMap(null);
-        markers.splice(idx, 1);
-        coordinates.splice(idx, 1);
-        window.localStorage.setItem('coordinates', JSON.stringify(coordinates));
-        // reRenderList();
-    });
-
-    const lat = document.createElement('span');
-    lat.textContent = `lat: ${coords.lat.toPrecision(8)}`;
-    lat.className = 'item__latlng';
-
-    const lng = document.createElement('span');
-    lng.textContent = `lng: ${coords.lng.toPrecision(8)}`;
-    lng.className = 'item__latlng';
-
-    itemHeader.appendChild(title);
-    itemHeader.appendChild(deleteBtn);
-
-    listItem.appendChild(itemHeader);
-    listItem.appendChild(lat);
-    listItem.appendChild(lng);
-
-    list.appendChild(listItem);
-}
 
 
 
 function initMap() {
     const options = {
-        zoom: 12,
+        zoom: 8,
         center: {
-            lat: 50.064744,
-            lng: 19.923261
+            lat: 50.065682,
+            lng: 19.941155
         }
     };
 
@@ -88,8 +47,52 @@ function initMap() {
         return marker;
     }
 
+    function addListPosition(coords, index) {
+
+        const listItem = document.createElement('div');
+        listItem.className = 'list__item';
+    
+        const itemHeader = document.createElement('div');
+        itemHeader.className = 'item__header';
+    
+        const title = document.createElement('h3');
+        title.textContent = `${index + 1}) coordinates:`;
+        title.className = 'item__title';
+    
+        const deleteBtn = document.createElement('img');
+        deleteBtn.src = './images/delete.png';
+        deleteBtn.className = 'item__deleteBtn';
+        deleteBtn.addEventListener('click', function (e) {
+            const idx = [...listItems].findIndex(el => el === this.parentNode.parentNode)
+            this.parentNode.parentNode.remove();
+            markers[idx].setMap(null);
+            markers.splice(idx, 1);
+            coordinates.splice(idx, 1);
+            window.localStorage.setItem('coordinates', JSON.stringify(coordinates));
+            reRenderList();
+        });
+    
+        const lat = document.createElement('span');
+        lat.textContent = `lat: ${coords.lat.toPrecision(8)}`;
+        lat.className = 'item__latlng';
+    
+        const lng = document.createElement('span');
+        lng.textContent = `lng: ${coords.lng.toPrecision(8)}`;
+        lng.className = 'item__latlng';
+    
+        itemHeader.appendChild(title);
+        itemHeader.appendChild(deleteBtn);
+    
+        listItem.appendChild(itemHeader);
+        listItem.appendChild(lat);
+        listItem.appendChild(lng);
+    
+        list.appendChild(listItem);
+    }
+
     function reRenderList() {
         list.textContent = '';
+        markers.forEach(marker => marker.setMap(null));
         markers = [];
         window.localStorage.setItem('coordinates', JSON.stringify(coordinates));
         coordinates.forEach((element, index) => {
@@ -100,6 +103,8 @@ function initMap() {
     }
 
     reRenderList();
+
+
 
 
     
